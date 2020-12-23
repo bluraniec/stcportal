da75d474806e11ce8b0a5cb691c08d3ba1767314
Bartosz Luraniec <46280104+bluraniec@users.noreply.github.com>
2020-12-23 16:50:06 +0100

    Add files via upload

---
+++ stcportal/stcportal/stcportal/admin.py
@@ -0,0 +1,89 @@
+from django import forms
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+
+from .models import User
+
+
+class AddUserForm(forms.ModelForm):
+    """
+    New User Form. Requires password confirmation.
+    """
+    password1 = forms.CharField(
+        label='Password', widget=forms.PasswordInput
+    )
+    password2 = forms.CharField(
+        label='Confirm password', widget=forms.PasswordInput
+    )
+
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'last_name')
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords do not match")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UpdateUserForm(forms.ModelForm):
+    """
+    Update User Form. Doesn't allow changing password in the Admin.
+    """
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = User
+        fields = (
+            'email', 'password', 'first_name', 'last_name', 'is_staff', 'is_active',
+            'read_only', 'subscribers_management', 'reporting_and_monitoring'
+        )
+
+    def clean_password(self):
+# Password can't be changed in the admin
+        return self.initial["password"]
+
+
+class UserAdmin(BaseUserAdmin):
+    form = UpdateUserForm
+    add_form = AddUserForm
+
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'read_only', 'subscribers_management', 'reporting_and_monitoring', 'is_active')
+    list_filter = ('is_active', 'is_staff')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Role', {'fields': ('is_staff', 'is_active')}),
+        # ('Active', {'fields': ('is_active')}),
+        ('Permissions', {'fields': ('read_only', 'subscribers_management', 'reporting_and_monitoring')}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                'classes': ('wide',),
+                'fields': (
+                    'email', 'first_name', 'last_name', 'password1',
+                    'password2', 'is_staff', 'read_only', 'subscribers_management', 'reporting_and_monitoring'
+                )
+            }
+        ),
+    )
+    search_fields = ('email', 'first_name', 'last_name', 'is_staff')
+    ordering = ('email', 'first_name', 'last_name', 'is_staff')
+    filter_horizontal = ()
+
+
+admin.site.register(User, UserAdmin)
